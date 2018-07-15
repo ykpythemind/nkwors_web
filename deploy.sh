9be0cb6ef1d1f15cc4cd23f99757457f7cc9f092
Yukito Ito <yukibukiyou@gmail.com>
2018-07-15 19:29:21 +0900

    ya

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ cp -r nare5 dist/
 cp -r shukujo dist/
 cp -r synapse dist/
 cp -r sound-recruiting dist/
+cp -r web/img dist/img/
+
 yarn run s3-deploy './dist/**' --cwd './dist/' --region us-west-2 --bucket nkwors.com
 
