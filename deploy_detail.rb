0c3fcb13085fe10846dc9a1f1cf8efad4713bb1c
Yukito Ito <yukibukiyou@gmail.com>
2018-07-16 21:33:12 +0900

    line 5

--- deploy_detail.rb
+++ deploy_detail.rb
@@ -6,7 +6,7 @@ end
 
 abort "SLACK_URL is not implemented" unless slack_url
 
-git_log = `git log --date=iso --pretty=format:"[%ad] %h %an : %s" | head`
+git_log = `git log --date=iso --pretty=format:"[%ad] %h %an : %s" | head -n 5`
 
 str = "deploy finished! \n\n  heading git log...\n #{git_log.chomp}"
 
