5c00158e0927c3b6b69ddf3e5996711042a27a67
Yukito Ito <yukibukiyou@gmail.com>
2018-07-16 20:58:43 +0900

    add notify

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,16 @@ set -eu
 
 rm -rf dist # clean up
 yarn run build
+
+# parcel管理外のフォルダをコピー
 cp -r nare5 dist/
 cp -r shukujo dist/
 cp -r synapse dist/
 cp -r sound-recruiting dist/
+
 cp -r web/img dist/img/ # parcel で管理されていない静的ファイル
 
 yarn run s3-deploy './dist/**' --cwd './dist/' --region us-west-2 --bucket nkwors.com
+
+ruby deploy_detail.rb
 
---
+++ deploy_detail.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+def slack_url
+  ENV['SLACK_URL']
+end
+
+abort "SLACK_URL is not implemented" unless slack_url
+
+git_log = `git log --date=iso --pretty=format:"[%ad] %h %an : %s" | head`
+
+str = "deploy finished! \n\n  heading git log...\n #{git_log.chomp}"
+
+`curl -X POST --data-urlencode 'payload={"text": "#{str}"}' #{slack_url}`
+
