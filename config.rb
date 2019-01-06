0f27779a3cb7800cc9cec3b042b35e04bea914d5
Yukito Ito <yukibukiyou@gmail.com>
2019-01-07 02:22:31 +0900

    s3sync

--- config.rb
+++ config.rb
@@ -41,6 +41,24 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'nkwors-staging' # The name of the S3 bucket you are targeting. This is globally unique.
+  s3_sync.region                     = 'ap-northeast-1'     # The AWS region for your bucket.
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.delete                     = true # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end
+
 configure :build do
   activate :minify_css
   # activate :minify_javascript
