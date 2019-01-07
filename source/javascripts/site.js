224d391f163eddf3b70727456e8119e4cdc65d63
Yukito Ito <yukibukiyou@gmail.com>
2019-01-07 12:08:46 +0900

    toggle

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,19 @@
 // This is where it all goes :)
+
+window.onload = function() {
+  var $button = document.getElementById("hambutton")
+  var isOpen = false
+
+  var toggle = function(open) {
+    if (open) {
+    } else {
+    }
+  }
+
+  $button.addEventListener('click', function() {
+    isOpen = !isOpen;
+    $button.classList.toggle("is-active")
+
+    toggle(isOpen);
+  })
+}
