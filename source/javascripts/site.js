b9e99764899493117957c92b18e6f521ee1f7a79
Yukito Ito <yukibukiyou@gmail.com>
2019-01-07 15:48:11 +0900

    dirty menu

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,11 +2,25 @@
 
 window.onload = function() {
   var $button = document.getElementById("hambutton")
+  var $menu = document.getElementById("mobilemenu")
   var isOpen = false
 
   var toggle = function(open) {
+    $menu.classList.remove("animated");
+
+    // TODO fix
     if (open) {
+      $menu.classList.remove("fadeOut");
+      $menu.classList.add("menuon");
+      $menu.classList.add("animated");
+      $menu.classList.add("fadeIn");
     } else {
+      $menu.classList.remove("fadeIn");
+      $menu.classList.add("animated");
+      $menu.classList.add("fadeOut");
+      setTimeout(function() {
+        $menu.classList.remove("menuon");
+      }, 1000)
     }
   }
 
