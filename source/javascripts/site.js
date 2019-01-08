facec2eeab235bdd2e684d74b3c66d9576a5f2a2
Yukito Ito <yukibukiyou@gmail.com>
2019-01-08 17:52:21 +0900

    scroll lock

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,20 +1,24 @@
-// This is where it all goes :)
+//= require body-scroll-lock/lib/bodyScrollLock.js
 
 window.onload = function() {
   var $button = document.getElementById("hambutton")
   var $menu = document.getElementById("mobilemenu")
+
   var isOpen = false;
   var blocking = false;
+
+  var noScroll = function(e) { e.preventDefault(); };
 
   var toggle = function() {
     if (blocking) {
       return
     }
     blocking = true
-    $menu.classList.remove("animated");
 
     // TODO fix
     if (isOpen) {
+      bodyScrollLock.disableBodyScroll($menu);
+
       $button.classList.add("is-active")
       $menu.classList.remove("fadeOut");
       $menu.classList.add("menuon");
@@ -22,6 +26,8 @@ window.onload = function() {
       $menu.classList.add("fadeIn");
       blocking = false;
     } else {
+      bodyScrollLock.enableBodyScroll($menu);
+
       $button.classList.remove("is-active")
       $menu.classList.remove("fadeIn");
       $menu.classList.add("animated");
